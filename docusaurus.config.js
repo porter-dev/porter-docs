35d4af74eed3012bcdb015f2b5d37378fd2e387a
Alexander Belanger <belanger@sas.upenn.edu>
2021-12-22 12:42:05 -0500

    first version with high level organization

---
+++ docusaurus.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  title: "Porter",
+  tagline: "Kubernetes powered PaaS that runs in your own cloud.",
+  url: "https://docs.porter.run",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "https://i.ibb.co/HnSk02f/ptr.png",
+  organizationName: "porter-dev",
+  projectName: "porter",
+  themeConfig: {
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    image: "/img/logo.png",
+    metaImage: "/img/logo.png",
+    navbar: {
+      hideOnScroll: true,
+      title: "Porter Documentation",
+      logo: {
+        alt: "Porter",
+        src: "/img/logo.png",
+        srcDark: "/img/logo.png",
+      },
+      items: [],
+    },
+    footer: {},
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+  scripts: [{ src: "/scripts/telemetry.js" }],
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "home-doc", // this allows us to set home page to docs
+        path: "docs",
+        routeBasePath: "/",
+        include: ["**/*.md", "**/*.mdx"],
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+  ],
+};
